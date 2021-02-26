893fb1ca959274466b563f90d76f39f801dad763
rocioperez <rocio.perez@ironhack.com>
2021-02-25 21:19:35 -0600

    typo fixed

--- models/Mentee.js
+++ models/Mentee.js
@@ -1,17 +1,21 @@
 const { Schema, model } = require("mongoose");
 
-const menteeSchema = new Schema({
-  objective: {
-    type: String,
-    enum: [
-      "Revisión de CV",
-      "Revisión de Porafolio",
-      "Entrevista de Trabajo",
-      "Otro",
-    ],
-    required: [true, "Selecciona al menos una opción"],
-    boooking: String,
-    chat: String,
-}, { timestamps: true });
+const menteeSchema = new Schema(
+  {
+    objective: {
+      type: String,
+      enum: [
+        "Revisión de CV",
+        "Revisión de Porafolio",
+        "Entrevista de Trabajo",
+        "Otro",
+      ],
+      required: [true, "Selecciona al menos una opción"],
+      boooking: String,
+      chat: String,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = model("Mentee", mentorSchema);
