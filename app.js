a69c0bf793e68615ca89647af8d73195e80e3449
rocioperez <rocio.perez@ironhack.com>
2021-03-06 16:51:48 -0600

    erased view references from app.js

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const path = require("path");
-
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
@@ -27,11 +25,6 @@ mongoose
     console.error("Error connecting to mongo", err);
   });
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
-);
-
 const app = express();
 
 // Middleware Setup
@@ -39,9 +32,6 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
 
 // Enable authentication using session + passport
 app.use(
